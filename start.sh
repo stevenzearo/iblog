0dbb172f717de0f2f3ead7218eed11af79ee77f1
steve <stevezou@chancetop.com>
2020-03-19 18:01:53 +0800

    set dev and test settings

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-chmod +x ./gradlew
-./gradlew build
-./gradlew :backend:user-service:build
-./gradlew :backend:user-service:overrideDevResources
-./gradlew :backend:user-service:flywayBaseline
-./gradlew :backend:user-service:flywayMigrate
-./gradlew :backend:user-service:run
---
+++ testStart.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+chmod +x ./gradlew
+./gradlew build
+./gradlew :backend:user-service:build
+./gradlew :backend:user-service:overrideTestResources
+./gradlew :backend:user-service-db-migration:overrideTestResources
+./gradlew :backend:user-service-db-migration:flywayBaseline
+./gradlew :backend:user-service-db-migration:flywayMigrate
+./gradlew :backend:user-service:run
