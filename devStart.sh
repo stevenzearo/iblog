ddf351f56c07eb3f50afebf43f5e20fa7e08e726
steve <stevezou@chancetop.com>
2020-03-20 18:12:03 +0800

    change application.yml settings

--- devStart.sh
+++ devStart.sh
@@ -6,4 +6,4 @@ chmod +x ./gradlew
 ./gradlew :backend:user-service-db-migration:overrideDevResources
 ./gradlew :backend:user-service-db-migration:flywayBaseline
 ./gradlew :backend:user-service-db-migration:flywayMigrate
-./gradlew :backend:user-service:run
+setsid ./gradlew :backend:user-service:run
