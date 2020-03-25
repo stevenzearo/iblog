ee4a44b28439d090e12b9cb55ae5439d5ea1ef73
steve <stevezou@chancetop.com>
2020-03-25 18:56:25 +0800

    fixed build file

--- backend/user-service/src/main/java/app/user/domain/User.java
+++ backend/user-service/src/main/java/app/user/domain/User.java
@@ -1,8 +1,10 @@
 package app.user.domain;
 
+
+import org.springframework.data.annotation.Id;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
