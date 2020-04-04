be7b919a51b5d72d862c3cb6b335c8791d367e51
stevevenzearo <2226069315@qq.com>
2020-04-04 16:33:01 +0800

    start bash modify

--- devStart.sh
+++ devStart.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 chmod +x ./gradlew
 ./gradlew build
-setside ./gradlew :frontend:registry-site:run
 ./gradlew :backend:user-service:overrideDevResources
 ./gradlew :backend:user-service-db-migration:overrideDevResources
-./gradlew :backend:user-service-db-migration:flywayBaseline
 ./gradlew :backend:user-service-db-migration:flywayMigrate
-setside ./gradlew :backend:user-service:run
-setside ./gradlew :frontend:bo-iblog-site:run
+nohup ./gradlew :frontend:registry-site:run &
+nohup ./gradlew :backend:user-service:run &
+nohup ./gradlew :frontend:bo-iblog-site:run &
--- testStart.sh
+++ testStart.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 chmod +x ./gradlew
 ./gradlew build
-./gradlew :frontend:registry-site:run
 ./gradlew :backend:user-service:overrideTestResources
 ./gradlew :backend:user-service-db-migration:overrideTestResources
-./gradlew :backend:user-service-db-migration:flywayBaseline
 ./gradlew :backend:user-service-db-migration:flywayMigrate
+./gradlew :frontend:registry-site:run
 ./gradlew :backend:user-service:run
 ./gradlew :frontend:bo-iblog-site:run
