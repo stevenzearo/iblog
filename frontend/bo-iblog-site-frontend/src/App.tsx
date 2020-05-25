ae681b1d5886635137a9472011f5dfcc50ca1155
steve <2226069315@qq.com>
2020-05-25 02:33:46 -0400

    frontend: rename index.tsx to router.tsx

--- frontend/bo-iblog-site-frontend/src/App.tsx
+++ frontend/bo-iblog-site-frontend/src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-
-import logo from './logo.svg';
 import './App.css';
 import LoginPage from './login'
 import RegisterPage from './register'
-import {UserWebService} from "./api/UserWebService";
-import {GetUserResponse} from "./api/user/GetUserResponse";
 
 function App() {
-    var userWebService: UserWebService = new UserWebService();
-    var data: GetUserResponse | null = userWebService.get(1);
     return (
         <div className="App">
             <header className="App-header">
