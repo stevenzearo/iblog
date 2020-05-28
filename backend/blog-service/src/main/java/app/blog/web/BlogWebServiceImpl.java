52192ff13035744280987882837e44db74c4fd8a
steve <2226069315@qq.com>
2020-05-28 05:48:53 -0400

    backend: get blog impl

--- backend/blog-service/src/main/java/app/blog/web/BlogWebServiceImpl.java
+++ backend/blog-service/src/main/java/app/blog/web/BlogWebServiceImpl.java
@@ -2,18 +2,11 @@ package app.blog.web;
 
 import app.blog.api.BlogWebService;
 import app.blog.api.blog.GetBlogResponse;
-import app.blog.dao.BlogDAO;
-import app.blog.domain.Blog;
-import app.blog.domain.Comment;
 import app.blog.service.BlogService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.time.ZonedDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author steve
