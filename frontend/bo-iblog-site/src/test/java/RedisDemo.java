70bdc3481f7830c0ce35bf712c76d1bc710ae14a
steve <2226069315@qq.com>
2020-09-02 06:15:04 -0400

    BS-002 Change admin info cache level from session level to context level

---
+++ frontend/bo-iblog-site/src/test/java/RedisDemo.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author steve
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class RedisDemo {
+
+    @Test
+    void test() {
+
+    }
+}
