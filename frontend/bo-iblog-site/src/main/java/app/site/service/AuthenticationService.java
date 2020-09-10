cccb4569f81f40f68d8a9e47be10d2cedc0db6e4
steve <2226069315@qq.com>
2020-09-10 07:37:52 -0400

    BS-002 Change admin info cache level from session level to context level

--- frontend/bo-iblog-site/src/main/java/app/site/service/AuthenticationService.java
+++ frontend/bo-iblog-site/src/main/java/app/site/service/AuthenticationService.java
@@ -3,7 +3,8 @@ package app.site.service;
 import app.site.web.Context;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ListOperations;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -15,7 +16,7 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class AuthenticationService {
     @Autowired
-    RedisTemplate<String, String> redisTemplate;
+    StringRedisTemplate redisTemplate;
 
     public String authentic() {
         ListOperations<String, String> stringListOperations = redisTemplate.opsForList();
@@ -23,5 +24,20 @@ public class AuthenticationService {
         stringListOperations.rightPush(Context.AUTHENTICATIONS, auth);
         redisTemplate.expire(Context.AUTHENTICATIONS, Context.AUTH_MINUTES, TimeUnit.MINUTES);
         return auth;
+    }
+
+    public boolean isValid(String authentication) {
+        SetOperations<String, String> opsForSet = redisTemplate.opsForSet();
+        Boolean existence = opsForSet.isMember(Context.AUTHENTICATIONS, authentication);
+        return existence == null ? false : existence;
+    }
+
+    public void expire(String authentication) {
+        SetOperations<String, String> opsForSet = redisTemplate.opsForSet();
+        opsForSet.remove(Context.AUTHENTICATIONS, authentication);
+    }
+
+    public void expireAll() {
+        redisTemplate.delete(Context.AUTHENTICATIONS);
     }
 }
