a79a97a39583c81419c411f947eab480a630f453
steve <2226069315@qq.com>
2020-10-13 04:17:33 -0400

    BS-005 Request and Response Restrictions improvement

---
+++ common/common-lib/src/main/java/app/validation/Size.java
@@ -0,0 +1,14 @@
+package app.validation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author steve
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Size {
+}
