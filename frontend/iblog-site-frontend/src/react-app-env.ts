f21969bd4b0bd2d29172b87b8dbdb15646cd8ffd
steve <2226069315@qq.com>
2020-10-15 21:20:52 +0800

    env settings change

--- frontend/iblog-site-frontend/src/react-app-env.ts
+++ frontend/iblog-site-frontend/src/react-app-env.ts
@@ -1,3 +1,3 @@
 /// <reference types="react-scripts" />
-export const SERVER_DOMAIN: string = "http://localhost:8411";
-export const WS_ENDPOINT_PREFIX: string = "ws://localhost:8411";
+export const SERVER_DOMAIN: string = "http://47.99.152.254:8411";
+export const WS_ENDPOINT_PREFIX: string = "ws://47.99.152.254:8411";
