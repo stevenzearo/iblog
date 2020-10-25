151010cae26cdfbb51dfad88e498ace76073c0b1
steve <2226069315@qq.com>
2020-10-25 23:06:28 +0800

    KafkaDemo

---
+++ backend/blog-service/src/test/java/app/blog/kafka/KafkaDemo.java
@@ -0,0 +1,35 @@
+package app.blog.kafka;
+
+import app.blog.IntegrationTest;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+/**
+ * @author steve
+ */
+@Component
+public class KafkaDemo extends IntegrationTest {
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
+
+    @Test
+    public void producerTest() {
+        kafkaTemplate.send("test-topic", "hello, world! helllllllloooooooo");
+    }
+
+    @KafkaListener(groupId = "test-group-id", topics = {"test-topic"})
+    public void listen(ConsumerRecord<String, String> record) {
+        Optional<String> valueOptional = Optional.ofNullable(record.value());
+        if (valueOptional.isEmpty()) return;
+
+        String s = valueOptional.get();
+        System.out.println(String.format("get message: %s", s));
+
+    }
+}
