b3e38436ae688c72844ec2dc87de09df65b8364a
steve <2226069315@qq.com>
2023-04-12 00:38:11 +0800

    fix mysql mock data script

--- backend/user-service/src/main/java/app/user/domain/AdminActivity.java
+++ backend/user-service/src/main/java/app/user/domain/AdminActivity.java
@@ -4,10 +4,12 @@ package app.user.domain;
 import app.validation.annotation.NotNull;
 import java.time.ZonedDateTime;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Index;
 import javax.persistence.Table;
 
+@Entity
 @Table(name = "admin_activities", indexes = {@Index(name = "ix_created_time", columnList = "created_time", unique = false)})
 public class AdminActivity {
     @Id
